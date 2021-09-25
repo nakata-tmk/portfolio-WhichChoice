a4680a732daaa5a675fd4f4386cc7a6b88e2429e
notte779 <mlp15amity@gmail.com>
2021-09-25 06:16:58 +0000

    Add_model:comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_060947) do
+ActiveRecord::Schema.define(version: 2021_09_25_061351) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_09_25_060947) do
     t.integer "user_id"
     t.integer "question_id"
     t.integer "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
