fed2c26a30a627cac127d0625dc45133158533c5
notte779 <mlp15amity@gmail.com>
2021-09-25 00:53:05 +0000

    Finish_public/homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+scope module: :public do
+  root to: 'homes#top'
+  get '/about' => 'homes#about', as: 'about'
+end
+
 end
