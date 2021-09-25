fe55c0732adafc26a8cec5513ed37a32e6ac9b5a
notte779 <mlp15amity@gmail.com>
2021-09-25 06:20:20 +0000

    Add_model:favorite

---
+++ db/migrate/20210925061745_create_favorites.rb
@@ -0,0 +1,12 @@
+class CreateFavorites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :favorites do |t|
+      t.integer :user_id
+      t.integer :question_id
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_061351) do
+ActiveRecord::Schema.define(version: 2021_09_25_061745) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_09_25_061351) do
     t.integer "user_id"
     t.integer "question_id"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
