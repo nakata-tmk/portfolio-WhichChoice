5559ed8e800f18aad708e09d33426e403e3168de
notte779 <mlp15amity@gmail.com>
2021-09-25 05:38:01 +0000

    Add_devise:admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :admins, path: 'admin/'
 
-scope module: :public do
-  root to: 'homes#top'
-  get '/about' => 'homes#about', as: 'about'
+  namespace :admin do
+    devise_for :admins, controllers: {
+      sessions: 'admin/admins/sessions',
+      registrations: 'admin/admins/registrations',
+      passwords: 'admin/admins/passwords'
+    }
+  end
 
-  devise_for :users, controllers: {
-    sessions: 'public/users/sessions',
-    registrations: 'public/users/registrations',
-    passwords: 'public/users/passwords'
-  }
-end
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+
+    devise_for :users, controllers: {
+      sessions: 'public/users/sessions',
+      registrations: 'public/users/registrations',
+      passwords: 'public/users/passwords'
+    }
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_041024) do
+ActiveRecord::Schema.define(version: 2021_09_25_051417) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "remember_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
