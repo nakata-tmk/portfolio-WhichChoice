948f1bd7ac1b53ea8df41bf45b725c6e8a04deab
notte779 <mlp15amity@gmail.com>
2021-09-25 05:10:46 +0000

    Add_devise:users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 scope module: :public do
   root to: 'homes#top'
   get '/about' => 'homes#about', as: 'about'
+
+  devise_for :users, controllers: {
+    sessions: 'public/users/sessions',
+    registrations: 'public/users/registrations',
+    passwords: 'public/users/passwords'
+  }
 end
 
 end
